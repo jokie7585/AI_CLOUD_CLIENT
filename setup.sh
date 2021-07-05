49d098b78595b2a9795c4489e579ddfc9b33b431
jokie7585 <tsaipenying@gmail.com>
2021-07-05 21:22:35 +0800

    fix setup port mapping

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 docker build -f="dockerfile" -t="devtooldocker7585/cetusclient:v1" .
-docker run --rm -d -p 3000:80/tcp devtooldocker7585/cetusclient:v1
+docker run --rm -d -p 80:80/tcp devtooldocker7585/cetusclient:v1
